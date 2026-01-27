2265edc2c96a687d7ae87699473568c53920beb8
bridge4er-startup <bridge4er@gmail.com>
2026-01-28 01:40:08 +0545

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+//This is the bakup file, all files merged at one place. Don't delete this file.
 // ==============================================
 // ENHANCED CACHE MANAGEMENT
 // ==============================================
