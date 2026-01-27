cf492c8a69de82d4b65465020bee328851b9e5e5
bridge4er-startup <bridge4er@gmail.com>
2026-01-28 01:35:52 +0545

    Update file-loading.js

--- assets/js/file-loading.js
+++ assets/js/file-loading.js
@@ -204,6 +204,18 @@ async function loadOldQuestionFiles() {
 // SEARCH FUNCTIONALITY
 // ==============================================
 
+function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
 function setupSearch() {
     const searchConfigs = [
         { 
